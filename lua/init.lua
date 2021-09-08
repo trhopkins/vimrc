ffa235c8f7d2519d32268ceda43f0593e00262d9
Travis Hopkins <trhopkins@protonmail.com>
2021-09-08 15:18:35 -0400

    Moved init.lua to own directory
    Currently uncertain if runtimepath is set up properly. Will test out
    moving init.lua and requiring other lua modules/source files.

--- lua/init.lua
+++ lua/init.lua
@@ -9,11 +9,12 @@ require "paq" {
 -- see oroques.dev/notes/neovim-init/
 
 -- HELPERS --
+-- consider adding these later for clarity
 
-local cmd = vim.cmd -- to execute Vim commands e.g. cmd('pwd')
-local fn = vim.fn   -- to call Vim functions e.g. fn.bufnr()
-local g = vim.g     -- a table to access global variables
-local opt = vim.opt -- to set options
+--local cmd = vim.cmd -- to execute Vim commands e.g. cmd('pwd')
+--local fn = vim.fn   -- to call Vim functions e.g. fn.bufnr()
+--local g = vim.g     -- a table to access global variables
+--local opt = vim.opt -- to set options
 
 local function map(mode, lhs, rhs, opts)
 	local options = {noremap = true}
