d4fedd57cc756bc83621ad6139b5f69993cdf33c
Travis Reid Hopkins <trhopkins@protonmail.com>
2022-03-05 11:02:26 -0500

    Check for paq before loading plugins

--- init.lua
+++ init.lua
@@ -1,9 +1,14 @@
 -- Travis Hopkins's Neovim configuration files
 -- Source lua/init.lua
 
-require('options') -- call options.lua to set editor configuration
-require('plugins') -- load necessary plugins. TODO: add more
+require('options') -- set editor configuration
 require('mappings') -- load mappings
-require('lsp') -- Language Server Protocol options
-require('treesitter') -- highlighting and syntax-edits?
+require('lsp') -- Language Server Protocol
+require('treesitter') -- highlighting and syntax-edits
+
+-- consider XDG_DATA_HOME ($HOME/.local/share)?
+paqLocation = os.getenv('HOME') .. '/.local/share/nvim/site/pack/paqs/start/paq-nvim'
+if os.execute('[ -d' .. paqLocation .. ' ]') == 0 then -- check if savq/paq-nvim is installed
+	require('plugins') -- load necessary plugins. TODO: add more
+end
 
