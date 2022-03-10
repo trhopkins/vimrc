a0cdaa8af913d7037146bb208630bad32119d812
Travis Reid Hopkins <trhopkins@protonmail.com>
2022-03-09 20:44:30 -0500

    Switched to nvim-cmp's default
    Trying out the original default editing modes again with more plugins
    enabled, to see if that appeases the editor. Maybe it is not being
    loaded in the correct order?

--- lua/completion.lua
+++ lua/completion.lua
@@ -1,47 +1,48 @@
 -- Set up completion engine
 
---[[
 -- set up lsp things. Check lsp.lua for more on this?
 local lspconfig = require('lspconfig')
 
 -- set up luasnip. Must happen before cmp setup
---local luasnip = require('luasnip') -- why does this break stuff?
+local luasnip = require('luasnip') -- why does this break stuff?
 
 -- add LSP 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
-local cmp = require 'cmp'
+-- Setup nvim-cmp.
+local cmp = require'cmp'
 
--- set up nvim-cmp. Taken directly from the docs at hrsh7th/nvim-cmp
 cmp.setup({
 	snippet = {
 		-- REQUIRED - you must specify a snippet engine
 		expand = function(args)
-			require('luasnip').lsp_expand(args.body)
+			--vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+			require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+			-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
 		end,
 	},
 	mapping = {
 		['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
 		['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
 		['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-		['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping
+		['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
 		['<C-e>'] = cmp.mapping({
 			i = cmp.mapping.abort(),
 			c = cmp.mapping.close(),
 		}),
-		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items
+		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 	},
 	sources = cmp.config.sources({
 		{ name = 'nvim_lsp' },
-		--{ name = 'nvim_lua' },
-		{ name = 'luasnip' },
+		{ name = 'luasnip' }, -- For luasnip users.
 	}, {
 		{ name = 'buffer' },
 	})
 })
 
--- Set configuration for specific filetype
+-- Set configuration for specific filetype.
 cmp.setup.filetype('gitcommit', {
 	sources = cmp.config.sources({
 		{ name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it. 
@@ -50,14 +51,14 @@ cmp.setup.filetype('gitcommit', {
 	})
 })
 
--- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore)
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline('/', {
 	sources = {
 		{ name = 'buffer' }
 	}
 })
 
--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore)
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(':', {
 	sources = cmp.config.sources({
 		{ name = 'path' }
@@ -66,76 +67,10 @@ cmp.setup.cmdline(':', {
 	})
 })
 
--- Setup lspconfig
+-- Setup lspconfig.
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
--- Add all language servers to square brackets
-require('lspconfig')['hls'].setup {
+-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
 	capabilities = capabilities
 }
-
---]]
-
--- Add additional capabilities supported by nvim-cmp
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
-
-local lspconfig = require('lspconfig')
-
--- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-local servers = { 'hls' }
-for _, lsp in ipairs(servers) do
-	lspconfig[lsp].setup {
-		-- on_attach = my_custom_on_attach,
-		capabilities = capabilities,
-	}
-end
-
--- luasnip setup
-local luasnip = require 'luasnip'
-
--- nvim-cmp setup
-local cmp = require 'cmp'
-cmp.setup {
-	snippet = {
-		expand = function(args)
-			require('luasnip').lsp_expand(args.body)
-		end,
-	},
-	mapping = {
-		['<C-p>'] = cmp.mapping.select_prev_item(),
-		['<C-n>'] = cmp.mapping.select_next_item(),
-		['<C-d>'] = cmp.mapping.scroll_docs(-4),
-		['<C-f>'] = cmp.mapping.scroll_docs(4),
-		['<C-Space>'] = cmp.mapping.complete(),
-		['<C-e>'] = cmp.mapping.close(),
-		['<CR>'] = cmp.mapping.confirm {
-			behavior = cmp.ConfirmBehavior.Replace,
-			select = true,
-		},
-		['<Tab>'] = function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expand_or_jumpable() then
-				vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
-			else
-				fallback()
-			end
-		end,
-		['<S-Tab>'] = function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
-			else
-				fallback()
-			end
-		end,
-	},
-	sources = {
-		{ name = 'nvim_lsp' },
-		{ name = 'luasnip' },
-	},
-}
-
--- nvim-cmp keeps glitching out and I have no idea why
 
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -6,9 +6,9 @@ require "paq" {
 	"nvim-treesitter/nvim-treesitter"; -- syntax highlighting
 	"hrsh7th/nvim-cmp";        -- autocompletion
 	"hrsh7th/cmp-nvim-lsp";    -- LSP source for cmp
-	--"hrsh7th/cmp-nvim-lua";    -- nvim-lua-specific source for cmp
-	--"hrsh7th/cmp-path";    -- file path source for cmp
-	--"hrsh7th/cmp-buffer";    -- buffer source for cmp (Markdown?)
+	"hrsh7th/cmp-nvim-lua";    -- nvim-lua-specific source for cmp
+	"hrsh7th/cmp-path";    -- file path source for cmp
+	"hrsh7th/cmp-buffer";    -- buffer source for cmp (Markdown?)
 	"saadparwaiz1/cmp_luasnip"; -- snippets source for cmp
 	"L3MON4D3/LuaSnip";        -- LSP snippets engine
 	"tpope/vim-surround";      -- wrap text automatically
@@ -19,5 +19,5 @@ require "paq" {
 	--"sirver/ultisnips";
 }
 
-require('completion') -- set up nvim-cmp and associated plugins further
+--require('completion') -- set up nvim-cmp and associated plugins further
 
